324bd341a927183cd7febf33e7aacb444d6170df
aetorres <andres.torresduran@gmail.com>
2021-05-18 13:31:51 -0600

    updating flux

--- apps/fluxcd/main.tf
+++ apps/fluxcd/main.tf
@@ -38,8 +38,18 @@ data "aws_caller_identity" "current" {}
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  #token                  = data.aws_eks_cluster_auth.cluster.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      data.terraform_remote_state.project_eks_infra.outputs.project_eks_cluster_id
+    ]
+  }
 }
 
 
