81b35336bb4f7b3908b8b5aa9ad696f08b2d0264
aetorres <andres.torresduran@gmail.com>
2021-05-18 17:33:59 -0600

    updating flux and kubeconfig

--- apps/fluxcd/main.tf
+++ apps/fluxcd/main.tf
@@ -38,18 +38,6 @@ data "aws_caller_identity" "current" {}
 
 provider "kubernetes" {
   config_path = "~/.kube/config"
-  #cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  #token                  = data.aws_eks_cluster_auth.cluster.token
-  #exec {
-  #  api_version = "client.authentication.k8s.io/v1alpha1"
-  #  command     = "aws"
-  #  args = [
-  #    "eks",
-  #    "get-token",
-  #    "--cluster-name",
-  #    data.terraform_remote_state.project_eks_infra.outputs.project_eks_cluster_id
-  #  ]
-  #}
 }
 
 
