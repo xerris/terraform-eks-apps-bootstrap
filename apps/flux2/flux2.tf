b23d6b2e62662d274b7ebcbe46c6dbe8bcddf815
aetorres <andres.torresduran@gmail.com>
2021-05-28 12:14:06 -0600

    updating flux2

--- apps/flux2/flux2.tf
+++ apps/flux2/flux2.tf
@@ -176,7 +176,7 @@ resource "github_repository_deploy_key" "main" {
   title      = "flux-${local.flux2["create_github_repository"] ? github_repository.main[0].name : local.flux2["repository"]}-${local.flux2["branch"]}"
   repository = local.flux2["create_github_repository"] ? github_repository.main[0].name : data.github_repository.main[0].name
   key        = tls_private_key.identity[0].public_key_openssh
-  read_only  = !local.flux2["auto_image_update"]
+  read_only  = local.flux2["auto_image_update"]
   depends_on = [ tls_private_key.identity ]
 }
 
