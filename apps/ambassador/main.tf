0a6f35f015631ae5b54ad26ba7c0225537c7f8e2
aetorres <andres.torresduran@gmail.com>
2021-08-05 15:58:46 -0600

    updating ambassador chart version

--- apps/ambassador/main.tf
+++ apps/ambassador/main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version       = "2.2.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.10.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+  }
+}
+
 resource "kubernetes_namespace" "ambassador" {
   metadata {
     annotations = {
@@ -18,7 +35,7 @@ resource "helm_release" "ambassador" {
   name       = "ambassador-elb"
   repository = "https://getambassador.io"
   chart      = "datawire/ambassador"
-  version    = "6.3.5"
+  #version    =  "6.7.11"#"6.3.5"
   namespace  = kubernetes_namespace.ambassador.metadata[0].name
   #repository = "https://getambassador.io/"
   timeout    = 3600
