50184f9d1958667ca52cd6b3deadd3e5af7584a8
aetorres <andres.torresduran@gmail.com>
2021-08-05 20:52:28 -0600

    adding experiment addon to terraform to use CRDs on manifests

--- apps/ambassador/main.tf
+++ apps/ambassador/main.tf
@@ -16,7 +16,10 @@ terraform {
 }
 
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+    experiments {
+        manifest_resource = true
+    }
+    config_path = "~/.kube/config"
 }
 
 provider "helm" {
@@ -27,11 +30,39 @@ provider "helm" {
 
 resource "helm_release" "ambassador" {
   name       = "ambassador-elb"
-  chart      = "https://app.getambassador.io/helm/ambassador-6.3.5.tgz"
+  chart      = "https://app.getambassador.io/helm/ambassador-6.3.5.tgz" #ambassador-operator-0.3.0.tgz
   namespace  = "ambassador"
   create_namespace = true
   timeout    = 3600
   values = [
     "${file("apps/ambassador/ambassador.yaml")}"
   ]
+}
+
+resource "kubernetes_manifest" "ambassador_host"{
+    depends_on = [ helm_release.ambassador]
+    manifest = {
+        "apiVersion" = "getambassador.io/v2"
+        "kind" = "Host"
+        "metadata" = {
+            "name" =  "ambassador"
+            "namespace" =  "ambassador"
+        }
+        "spec" = {
+            "hostname" = "*"
+            "selector" = {
+                "matchLabels" = {
+                    "hostname" = "wildcard"
+                }
+            }
+            "acmeProvider" = {
+                "authority" = "none"
+            }
+            "requestPolicy" = {
+                "insecure" = {
+                    "action" = "Route"
+                }
+            }
+        }
+    }
 }
