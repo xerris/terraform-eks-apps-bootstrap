f456e7f4a37f2a245f7d805b4c17d55e5a471072
aetorres <andres.torresduran@gmail.com>
2021-09-09 13:06:08 -0600

    adding prometheus-adaptor (HPA)

--- apps/prometheus-operator/main.tf
+++ apps/prometheus-operator/main.tf
@@ -60,3 +60,41 @@ resource "helm_release" "prometheus-operator" {
     value = "true"
   }
 }
+
+resource "kubernetes_namespace" "custom-metrics" {
+  metadata {
+    annotations = {
+      name = "custom-metrics"
+    }
+    labels = {
+      namespace = "custom-metrics"
+      }
+    name = "custom-metrics"
+  }
+}
+
+resource "helm_release" "prometheus-adaptor" {
+  depends_on = [kubernetes_namespace.custom-metrics,helm_release.prometheus-operator ]
+  name = "prometheus-adaptor"
+  version   = "0.8.4"
+  chart      = "prometheus-adapter"
+  repository = "https://prometheus-community.github.io/helm-charts/"
+  namespace  = kubernetes_namespace.custom-metrics.metadata[0].name
+  timeout    = 3600
+  dependency_update = true
+  values = []
+  set {
+    name  = "prometheus.url"
+    value = "http://prometheus-operator-operator"
+  }
+  set {
+    name  = "prometheus.port"
+    value = "9090"
+  }
+
+  set {
+    name  = "rbac.create"
+    value = "true"
+  }
+
+}
